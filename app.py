27b3d8237bd35722d89d2fa15c35b278e0e0c7e3
Unforgery <contact@unforgery.fr>
2026-04-20 14:33:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -88,4 +88,4 @@ Return JSON:
         return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
